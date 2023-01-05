f2fe1ea8cddf5635c9024723dd64b4963c74a2f6
Jacksonckc <jackson858216047@gmail.com>
2023-01-05 13:04:21 -0700

    first commit

--- 341-lesson-1/server.js
+++ 341-lesson-1/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.listen(3000, () => {
-  console.log('starts');
+  console.log('The server has started!');
 });
 
 app.get('/name', (req, res) => {
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CSE-341
+
+This repository contains all the course work for CSE 341
