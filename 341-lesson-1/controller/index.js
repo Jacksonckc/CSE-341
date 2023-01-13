b212d2be83b9e3809e15ae026882cf002675c0c3
Jacksonckc <jackson858216047@gmail.com>
2023-01-13 09:26:11 -0700

    1

--- 341-lesson-1/controller/index.js
+++ 341-lesson-1/controller/index.js
@@ -17,12 +17,21 @@ const getMongoData = async (req, res, next) => {
   });
 };
 
+const updateDoc = async (req, res, next) => {
+  const result = await mongodb
+    .getDb()
+    .db('sample_airbnb')
+    .collection('listingsAndReviews')
+    .updateOne(
+      { name: 'Modern Spacious 1 Bedroom Loft' },
+      { $set: { bedrooms: 90 } }
+    );
+  console.log(result.matchedCount);
+  console.log(result.modifiedCount);
+  return res.json(result);
+};
+
 const getAllCollections = async (req, res, next) => {
-  const newUser = {
-    name: 'Jackson',
-    gender: 'male',
-    age: 90,
-  };
   const cursor = await mongodb
     .getDb()
     .db('sample_airbnb')
@@ -40,36 +49,22 @@ const getAllCollections = async (req, res, next) => {
 
   const result = await cursor.toArray();
 
-  result.forEach((element) => {
-    console.log(
-      element.name,
-      element.bedrooms,
-      element.bathrooms,
-      element.last_review
-    );
-  });
+  // result.forEach((element) => {
+  //   console.log(
+  //     element.name,
+  //     element.bedrooms,
+  //     element.bathrooms,
+  //     element.last_review
+  //   );
+  // });
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json(result);
-};
-
-const updateDoc = async (req, res, next) => {
-  const result = await mongodb
-    .getDb()
-    .db('sample_airbnb')
-    .collection('listingsAndReviews')
-    .updateOne(
-      { name: 'Modern Spacious 1 Bedroom Loft' },
-      { $set: { bedrooms: 90 } }
-    );
-  console.log(result.matchedCount);
-  console.log(result.modifiedCount);
-  return res.json(result);
 };
 
 const getContacts = async (req, res, next) => {
   const cursor = await mongodb.getDb().db().collection('contact1').find();
   const result = await cursor.toArray();
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json(result);
 };
