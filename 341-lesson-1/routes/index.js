5f71fef5f85ba0ecb5bc7f15263f7ff356f7df85
Jacksonckc <jackson858216047@gmail.com>
2023-01-26 08:47:56 -0700

    1

--- 341-lesson-1/routes/index.js
+++ 341-lesson-1/routes/index.js
@@ -7,9 +7,6 @@ const swaggerDocs = require('../swagger.json');
 routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 routes.get('/', controller.getName);
 routes.get('/name', controller.getAnotherName);
-// routes.get('/mongoData', controller.getMongoData);
-// routes.get('/getAllCollections', controller.getAllCollections);
-// routes.put('/updateDoc', controller.updateDoc);
 
 routes.use('/contacts', require('./contacts'));
 
