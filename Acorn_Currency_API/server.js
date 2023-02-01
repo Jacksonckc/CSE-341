5c21bda30c5897e5d052e6396e82150b9a7ec84b
Jacksonckc <jackson858216047@gmail.com>
2023-02-01 13:44:40 -0700

    1

--- Acorn_Currency_API/server.js
+++ Acorn_Currency_API/server.js
@@ -3,19 +3,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-dotenv.config();
+// const Database = require('./src/db/database');
 
-const port: string = process.env.PORT || '8080';
+dotenv.config();
+// const db = new Database();
+
+const port = process.env.PORT || '8080';
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/', (req: any, res: any) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen('8080', (e: Error) => {
+app.listen('8080', (e) => {
   e ? console.log(e) : console.log(`The server is running on port: ${port}.`);
 });
-
-export default app;
